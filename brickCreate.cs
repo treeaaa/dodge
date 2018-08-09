3688ba38c697cb82eb2e3ff2d1d16c3a30de9d6a
treeaaa <42208206+treeaaa@users.noreply.github.com>
2018-08-09 11:43:39 +0800

    無邊界六邊形地磚拼接
    無邊界六邊形地磚拼接

---
+++ brickCreate.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brickCreate : MonoBehaviour {
+	public GameObject hexagon200x200;
+	Vector3 hexagonVector0,hexagonVector1,hexagonVector2,hexagonVector3,hexagonVector4,hexagonVector5;  //六邊形基本向量位移   用於拼接六邊形
+
+	Vector3[] hexagonVectorArray = new Vector3[6];		//物件陣列  基本向量
+	public static List<GameObject> hexagon200x200List = new List<GameObject>();			//地磚串  用於動態移動地磚
+	void Start () {
+		hexagonVector0.x = 1.5f*parameter.hexagonLength* (-1);				//左上
+		hexagonVector0.y = Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2) -parameter.hexagonLength)*(0.5f*parameter.hexagonLength);
+
+		hexagonVector1.x = 0f;
+		hexagonVector1.y = Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2) -parameter.hexagonLength);		//正上
+
+		hexagonVector2.x = 1.5f*parameter.hexagonLength;						//右上
+		hexagonVector2.y =Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2) -parameter.hexagonLength) *(0.5f*parameter.hexagonLength);	
+
+		hexagonVector3.x = 1.5f*parameter.hexagonLength;						//右下
+		hexagonVector3.y = Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2) -parameter.hexagonLength) * (-0.5f*parameter.hexagonLength);
+
+		hexagonVector4.x = 0f;						//正下
+		hexagonVector4.y =Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2)-parameter.hexagonLength) * (-1);
+
+		hexagonVector5.x = 1.5f *parameter.hexagonLength* (-1);					//左下
+		hexagonVector5.y =Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2)-parameter.hexagonLength) *(-0.5f*parameter.hexagonLength);
+
+		hexagonVectorArray[0] =hexagonVector2;//右上
+		hexagonVectorArray[1] =hexagonVector3;//右下
+		hexagonVectorArray[2] =hexagonVector4;//正下
+		hexagonVectorArray[3] =hexagonVector5;//左下
+		hexagonVectorArray[4] =hexagonVector0;//左上
+		hexagonVectorArray[5] =hexagonVector1;//正上
+		
+		CreateHexagon (parameter.hexagonBrickLayer);		
+	
+		
+	}
+
+
+	void Update () {
+		
+	}
+	protected void CreateHexagon(int layer){		//創造所有地磚
+		int layernow = 0;
+		Vector3 vectorNow = new Vector3 (0, 0, 0);
+		HexagonCreateAddToList (vectorNow);		//創造一個地磚並加入地磚list
+		while (layernow < layer) {
+			vectorNow += hexagonVectorArray [4];							// 左上 下一層
+			HexagonCreateAddToList (vectorNow);	
+			layernow++;														//層+1
+			for (int i = 0; i < layernow - 1; i++) {
+				vectorNow += hexagonVectorArray [5];		//正上		產生層數-1的量
+				HexagonCreateAddToList (vectorNow);	
+			}
+			for (int i = 0; i < 5; i++) {
+				for (int j = 0; j < layernow; j++) {
+					vectorNow += hexagonVectorArray [i];
+					HexagonCreateAddToList (vectorNow);	
+				}
+			}
+		}
+	}
+	protected void HexagonCreateAddToList(Vector3 vectorNow){	//創造一個地磚並加入地磚list
+		GameObject a= Instantiate (hexagon200x200, vectorNow, Quaternion.identity);
+		hexagon200x200List.Add (a);  //加入地磚串
+	}
+}
---
+++ brickMove.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brickMove : MonoBehaviour {
+	int brickCenter;		//中心點
+	int[] brickAround = new int[6];		//六個圍繞的地磚
+	float sqrt3 = Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2)-parameter.hexagonLength);
+	void Start () {
+		brickCenter = 0;						//在hexagon200x200List 預設0是中心   1~6=周圍六點
+		for (int i = 1; i < 7; i++) {
+			brickAround [i-1] = i;
+		}
+	}
+	void Update () {
+		for (int i = 0; i < brickAround.Length; i++) {
+			if (Vector3.Distance (parameter.playerXY, brickCreate.hexagon200x200List [brickAround [i]].transform.position) < sqrt3/2) {
+				Vector3 coorDinateA,coorDinateB,coorDinateCenter;	//座標A為原本為中心的座標  座標B為新中心的座標
+				float slope;			//斜率
+				float constantB;		//常數b
+				coorDinateA = brickCreate.hexagon200x200List[brickCenter].transform.position;		//座標A為原本為中心的座標
+				brickCenter = brickCreate.hexagon200x200List.IndexOf (brickCreate.hexagon200x200List [brickAround [i]]);		//取得索引值
+				coorDinateB = brickCreate.hexagon200x200List[brickCenter].transform.position;		//座標B為新中心的座標
+				coorDinateCenter =(coorDinateA +coorDinateB)/2;
+				slope = (-1)/((coorDinateB.y-coorDinateA.y) / (coorDinateB.x - coorDinateA.x)) ;		//算出斜率  垂直的那條 兩條垂直線  斜率相乘=-1
+				constantB = coorDinateCenter.y - slope*coorDinateCenter.x ;
+
+
+				int brickAroundIndex =0;
+				for(int j= 0;j< brickCreate.hexagon200x200List.Count;j++){
+					float abDistance = Vector3.Distance (brickCreate.hexagon200x200List [brickCenter].transform.position, brickCreate.hexagon200x200List [j].transform.position);
+					if(brickCenter!=j){								//不能抓自己
+						if(	1.5f<abDistance&&abDistance<2.5f){		//新的周圍六個邊
+							brickAround[brickAroundIndex] =brickCreate.hexagon200x200List.IndexOf (brickCreate.hexagon200x200List[j]);		//取得索引值
+							brickAroundIndex++;
+
+						}
+						if(abDistance>parameter.hexagonBrickLayer* Mathf.Sqrt (Mathf.Pow(2*parameter.hexagonLength,2)-parameter.hexagonLength)+0.1){	// 層數為5 實際產生6層
+							brickCreate.hexagon200x200List[j].transform.position = GetProjectionPoint(brickCreate.hexagon200x200List[j].transform.position,slope, -1f,constantB);									//不需要動的最大為parameter.hexagonBrickLayer*根號3
+						}																																	//+0.1為增加判斷範圍 但是不超過最大那一層
+					}
+				}
+			}
+		}
+	}
+	protected Vector3 GetProjectionPoint(Vector3 originalXY, float a,float b,float c){		//取得對對稱點    ax+by+c=0
+		Vector3 projectionPoint = new Vector3(0,0,0);
+		projectionPoint.x = originalXY.x - 2 * a * (a * originalXY.x + b * originalXY.y + c) / (Mathf.Pow (a, 2) + Mathf.Pow (b, 2));
+		projectionPoint.y = originalXY.y - 2 * b * (a * originalXY.x + b * originalXY.y + c) / (Mathf.Pow (a, 2) + Mathf.Pow (b, 2));
+		return projectionPoint;
+	}
+}
